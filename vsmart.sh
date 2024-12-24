3c6f81c683a6578255b833164a8215921b86638d
NasserTh <lusarawrc@yahoo.com>
2024-12-24 17:29:38 +0700

    Add files via upload

---
+++ vsmart.sh
@@ -0,0 +1,40 @@
+M="\e[31m"
+I=="\e[32m"
+K=="\e[34m"
+D=="\e[36m"
+echo ""
+echo "$K╔═══╦═══╦══╗╔╗──╔═══╦═══╦════╗"
+sleep 0.1
+echo "╚╗╔╗║╔══╣╔╗║║║──║╔═╗║╔═╗║╔╗╔╗║"
+sleep 0.1
+echo "─║║║║╚══╣╚╝╚╣║──║║─║║║─║╠╝║║╚╝"
+sleep 0.1
+echo "─║║║║╔══╣╔═╗║║─╔╣║─║║╚═╝║─║║"
+sleep 0.1
+echo "╔╝╚╝║╚══╣╚═╝║╚═╝║╚═╝║╔═╗║─║║"
+sleep 0.1
+echo "╚═══╩═══╩═══╩═══╩═══╩╝─╚╝─╚╝"
+echo "$D Debloat VSMART"
+sleep 3
+echo -e $M "Github $I https://github.com/Naserdh"
+sleep 3
+com.vsmart.android.movetovsmartnull #Chuyển Dữ Liệu 
+pm uninstall --user 0 com.google.android.apps.docs #Drive
+pm uninstall --user 0 com.android.fmradio #Đài FM
+pm uninstall --user 0 com.google.android.deskclock #Đồng hồ
+pm uninstall --user 0 com.google.android.googlequicksearchbox #Google
+pm uninstall --user 0 com.google.android.keep #Keep Ghi chú
+pm uninstall --user 0 com.google.android.apps.maps #Maps
+pm uninstall --user 0 com.android.wallpaper.livepickernull #Trình chọn Hình nền Động
+pm uninstall --user 0 com.google.android.apps.messaging #Tin nhắn
+pm uninstall --user 0 com.google.android.videos #Google TV
+pm uninstall --user 0 com.vinsmart.calendar #Lịch
+pm uninstall --user 0 com.vsmart.android.vsmartwarranty #Vsmart 
+pm uninstall --user 0 org.cyanogenmod.screencastnull #Quay màn hình
+pm uninstall --user 0 com.android.emergencynull #Thông tin khẩn cấp  
+pm uninstall --user 0 com.google.android.ttsnull #Nhận dạng và tổng hợp giọng nói của Google
+echo " Thanks for Using debloat"
+sleep 2
+echo ""
+echo "I will update new packages to optimize the experience."
+echo " Goodbye"
